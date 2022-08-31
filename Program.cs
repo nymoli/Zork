1ea9af714883bea2494210416cbc94d5e7aed30b
nymoli <nymolina@knights.ucf.edu>
2022-08-31 15:12:31 -0400

    zork 1.3

--- Program.cs
+++ Program.cs
@@ -8,47 +8,49 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
 
+            bool isRunning = true;
+
+            
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString = null;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        isRunning = false;
+                        outputString = "Thank you for playing.";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}.";
+                        break;
+
+                    case Commands.UNKNOWN:
+                        outputString = "Unknown command.";
+                        break;
+
+                }
+                
+                Console.WriteLine(outputString);
+            }
+           
         }
 
         private static Commands ToCommand(string commandString)
         {
-            Commands command;
-            switch (commandString)
-            {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
-
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-
-                default:
-                    command = Commands.UNKNOWN;
-                    break;
-            }
-
-            return command;
+            return Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         }
     }
 }
