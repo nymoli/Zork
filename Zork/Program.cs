772a173ba076156a10696a65b500024ec6517a1f
nymoli <nymolina@knights.ucf.edu>
2022-09-15 17:58:42 -0400

    Updated Zork 1.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,111 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom
-        {
-            get
-            {
-                return Rooms[Location.Row, Location.Column];
-            }
-        }
-        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            Commands command = Commands.UNKNOWN;                       
-            while (command != Commands.QUIT)
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+            if (inputString == "QUIT")
             {
-                Console.Write($"{CurrentRoom}\n> ");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                string outputString = null;
-                switch (command)
-                {
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing.";
-                        break;
-
-                    case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
-                        break;
-
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if (Move(command) == false)
-                        {
-                            outputString = "The way is shut!";
-                        }
-                        break;
-
-                    case Commands.UNKNOWN:
-                        outputString = "Unknown command.";
-                        break;
-
-                }
-                
-                Console.WriteLine(outputString);
+                Console.WriteLine("Thank you for playing.");
             }
-           
-        }
-
-        private static bool Move(Commands command)
-        {
-            Assert.IsTrue(IsDirection(command), "Invalid direction");
-
-            bool isValidMove = true;
-            switch (command)
+            else if (inputString == "LOOK")
             {
-                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
-                    Location.Row++;
-                    break;
-
-                case Commands.SOUTH when Location.Row > 0:
-                    Location.Row--;
-                    break;
-
-                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-                    Location.Column++;
-                    break;
-
-                case Commands.WEST when Location.Column > 0:
-                    Location.Column--;
-                    break;
-
-                default:
-                    isValidMove = false;
-                    break;
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door");
             }
-
-            return isValidMove;
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
-
-        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
-
-        private static bool IsDirection(Commands command) => Directions.Contains(command);
-
-        private static readonly string[,] Rooms =
-        {
-            { "Rocky Trail", "South of House", "Canyon View" },
-            { "Forest", "West of House", "Behind House" },
-            { "Dense Woods", "North of House", "Clearing" }
-        };
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST
-        };
-
-        private static (int Row, int Column) Location = (1, 1);
     }
 }
 
