4e3bbe381fb167df07880abe494da43b6ee20cb6
nymoli <nymolina@knights.ucf.edu>
2022-10-31 14:00:50 -0400

    Fixed Namespace Issue

--- Zork/Item.cs
+++ Zork/Item.cs
@@ -4,12 +4,15 @@
     {
         public string Name { get; }
 
-        public string Description { get; }
+        public string LookDescription { get; }
 
-        public Item(string name, string description)
+        public string InventoryDescription { get; }
+
+        public Item(string name, string lookDescription, string inventoryDescription)
         {
             Name = name;
-            Description = description;
+            LookDescription = lookDescription;
+            InventoryDescription = inventoryDescription;
         }
     }
 }
