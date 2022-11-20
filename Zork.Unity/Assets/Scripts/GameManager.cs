d191679d66964c377b46b64e90a18efadf5675f9
nymoli <nymolina@knights.ucf.edu>
2022-11-19 23:40:35 -0500

    Zork.Unity Update

--- Zork.Unity/Assets/Scripts/GameManager.cs
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -1,20 +1,53 @@
 using UnityEngine;
-using Zork;
+using Zork.Common;
+using Newtonsoft.Json;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI LocationText;
+
+    [SerializeField]
+    private TextMeshProUGUI ScoreText;
+
+    [SerializeField]
+    private TextMeshProUGUI MovesText;
+
+    [SerializeField]
+    private UnityInputService InputService;
+
+    [SerializeField]
+    private UnityOutputService OutputService;
+
+    private Game _game;
+
     private void Awake()
     {
-        
+        TextAsset gameJson = Resources.Load<TextAsset>("GameJson");
+        _game = JsonConvert.DeserializeObject<Game>(gameJson.text);
+        _game.Player.LocationChanged += Player_LocationChanged;
+        _game.Run(InputService, OutputService);
+        LocationText.text = _game.Player.CurrentRoom.Name;
+    }
+
+    private void Player_LocationChanged(object sender, Room location)
+    {
+        LocationText.text = location.Name;
     }
 
     void Start()
     {
-        
+        InputService.SetFocus();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            InputService.ProcessInput();
+            InputService.SetFocus();
+        }        
+
     }
 }
---
+++ Zork.Unity/Assets/Scripts/UnityOutputService.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Zork.Common;
+using System.Collections.Generic;
+
+public class UnityOutputService : MonoBehaviour, IOutputService
+{
+    [SerializeField]
+    private TextMeshProUGUI TextLinePrefab;
+
+    [SerializeField]
+    private Image NewLinePrefab;
+
+    [SerializeField]
+    private Transform ContentTransform;
+
+    public void Write(object obj) => ParseAndWriteLine(obj.ToString());
+
+    public void Write(string message) => ParseAndWriteLine(message);
+
+
+    public void WriteLine(object obj) => ParseAndWriteLine(obj.ToString());
+
+
+    public void WriteLine(string message) => ParseAndWriteLine(message);
+
+    private void ParseAndWriteLine(string message)
+    {
+        var textLine = Instantiate(TextLinePrefab, ContentTransform);
+        textLine.text = message;
+        _entries.Add(textLine.gameObject);
+    }
+
+    private List<GameObject> _entries = new List<GameObject>();
+}
